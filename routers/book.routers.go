f73753a5cf8c9c1147b04d83883a5a5a488f6679
emixbal <emixbal@gmail.com>
2021-11-02 15:23:10 +0700

    ok

--- routers/book.routers.go
+++ routers/book.routers.go
@@ -2,17 +2,14 @@ package routers
 
 import (
 	"fiber-gorm/controllers"
+	"fiber-gorm/middlewares"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func Book(app *fiber.App) {
 	user := app.Group("/books")
-	user.Get("/",
-		controllers.FetchAllBooks,
-	)
 
-	user.Post("/",
-		controllers.CreateBook,
-	)
+	user.Get("/", middlewares.ExampleMiddleware, controllers.FetchAllBooks) // contoh menggunakan middleware
+	user.Post("/", controllers.CreateBook)
 }
