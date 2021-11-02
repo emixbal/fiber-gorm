f97798e5107229b0ad322cf9c4d98d22256f8500
emixbal <emixbal@gmail.com>
2021-11-02 17:48:59 +0700

    add DB_DRIVER setup

--- config/gorm.config.go
+++ config/gorm.config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"fmt"
 	"os"
 
 	"gorm.io/driver/mysql"
@@ -13,13 +14,17 @@ var (
 )
 
 func InitDB() *gorm.DB {
-	DSN := os.Getenv("DB_USERNAME") + ":" + os.Getenv("DB_PASSWORD") + "@tcp(" + os.Getenv("DB_HOST") + ":" + os.Getenv("DB_PORT") + ")/" + os.Getenv("DB_NAME") + "?charset=utf8mb4&parseTime=True&loc=Local"
-	DB, err = gorm.Open(mysql.Open(DSN), &gorm.Config{})
+	if os.Getenv("DB_DRIVER") == "mysql" {
+		DSN := os.Getenv("DB_USERNAME") + ":" + os.Getenv("DB_PASSWORD") + "@tcp(" + os.Getenv("DB_HOST") + ":" + os.Getenv("DB_PORT") + ")/" + os.Getenv("DB_NAME") + "?charset=utf8mb4&parseTime=True&loc=Local"
+		DB, err = gorm.Open(mysql.Open(DSN), &gorm.Config{})
 
-	if err != nil {
-		panic("connectionString error")
+		if err != nil {
+			panic("connectionString error")
+		}
+		return DB
 	}
-	return DB
+	fmt.Println("DB_DRIVER not supported")
+	return nil
 }
 
 func GetDBInstance() *gorm.DB {
